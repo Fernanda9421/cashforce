06526e39b81d8486582904a8b349e0d3d4a0d193
Fernanda Andrade de Carvalho <fer.candrade21@gmail.com>
2022-12-27 22:15:13 +0000

    feat: cria middleware de erro

---
+++ cashforce_backend/src/middlewares/error.middleware.ts
@@ -0,0 +1,11 @@
+import { NextFunction, Request, Response } from 'express';
+import HttpException from '../exceptions/HttpException';
+
+function errorMiddleware(error:HttpException, _req:Request, res:Response, _next:NextFunction):void {
+  const status = error.status || 500;
+  const message = error.message || 'Internal server error!';
+
+  res.status(status).send(message);
+}
+
+export default errorMiddleware;
