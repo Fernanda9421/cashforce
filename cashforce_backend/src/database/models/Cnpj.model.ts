08f780c6f90774c3d56d575eab607dc483920ca1
Fernanda Andrade de Carvalho <fer.candrade21@gmail.com>
2022-12-27 20:59:11 +0000

    feat: cria Model - Cnpj

---
+++ cashforce_backend/src/database/models/Cnpj.model.ts
@@ -0,0 +1,24 @@
+import { Model, DataTypes } from 'sequelize';
+import db from '.';
+
+class Cnpj extends Model {
+  declare id: number;
+  declare cnpj: string;
+  declare companyType: string;
+  declare createdAt: string;
+  declare updatedAt: string;
+}
+
+Cnpj.init({
+  id: { type: DataTypes.INTEGER, allowNull: false, primaryKey: true, autoIncrement: true },
+  cnpj: { type: DataTypes.STRING, allowNull: false, unique: true },
+  companyType: { type: DataTypes.STRING, allowNull: false },
+  createdAt: { type: DataTypes.DATE, allowNull: false },
+  updatedAt: { type: DataTypes.DATE, allowNull: false },
+}, {
+  sequelize: db,
+  modelName: 'Cnpj',
+  underscored: true,
+});
+
+export default Cnpj;
