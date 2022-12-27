0618fe9ac5aa6d0ec78f39cb953320be5e9671d2
Fernanda Andrade de Carvalho <fer.candrade21@gmail.com>
2022-12-27 21:33:09 +0000

    feat: cria Model - Provider

---
+++ cashforce_backend/src/database/models/Provider.model.ts
@@ -0,0 +1,73 @@
+import { Model, DataTypes } from 'sequelize';
+import db from '.';
+import Cnpj from './Cnpj.model';
+
+class Provider extends Model {
+  declare id: number;
+  declare name: string;
+  declare tradingName: string;
+  declare cashforceTax: string;
+  declare responsibleName: string;
+  declare responsibleEmail: string;
+  declare responsiblePosition: string;
+  declare responsiblePhone: string;
+  declare responsibleMobile: string;
+  declare website: string;
+  declare postalCode: string;
+  declare address: string;
+  declare number: string;
+  declare complement: string;
+  declare neighborhood: string;
+  declare city: string;
+  declare state: string;
+  declare bank: string;
+  declare bankAgency: string;
+  declare account: string;
+  declare documents: string;
+  declare phoneNumber: string;
+  declare situation: string;
+  declare situationDate: string;
+  declare createdAt: string;
+  declare updatedAt: string;
+  declare cnpjId: number;
+  declare email: string;
+}
+
+Provider.init({
+  id: { type: DataTypes.INTEGER, allowNull: false, primaryKey: true, autoIncrement: true },
+  name: { type: DataTypes.STRING, allowNull: false },
+  tradingName: { type: DataTypes.STRING },
+  cashforceTax: { type: DataTypes.STRING },
+  responsibleName: { type: DataTypes.STRING },
+  responsibleEmail: { type: DataTypes.STRING },
+  responsiblePosition: { type: DataTypes.STRING },
+  responsiblePhone: { type: DataTypes.STRING },
+  responsibleMobile: { type: DataTypes.STRING },
+  website: { type: DataTypes.STRING },
+  postalCode: { type: DataTypes.STRING },
+  address: { type: DataTypes.STRING },
+  number: { type: DataTypes.STRING },
+  complement: { type: DataTypes.STRING },
+  neighborhood: { type: DataTypes.STRING },
+  city: { type: DataTypes.STRING },
+  state: { type: DataTypes.STRING },
+  bank: { type: DataTypes.STRING },
+  bankAgency: { type: DataTypes.STRING },
+  account: { type: DataTypes.STRING },
+  documents: { type: DataTypes.STRING },
+  phoneNumber: { type: DataTypes.STRING },
+  situation: { type: DataTypes.STRING },
+  situationDate: { type: DataTypes.STRING },
+  createdAt: { type: DataTypes.DATE, allowNull: false },
+  updatedAt: { type: DataTypes.DATE, allowNull: false },
+  cnpjId: { type: DataTypes.INTEGER },
+  email: { type: DataTypes.STRING },
+}, {
+  sequelize: db,
+  modelName: 'Provider',
+  underscored: true,
+});
+
+Provider.belongsTo(Cnpj, { foreignKey: 'cnpjId', as: 'cnpjs' });
+
+export default Provider;
