058c5fe015e84ee1db79da68abedbfba4544466d
Fernanda Andrade de Carvalho <fer.candrade21@gmail.com>
2022-12-27 22:17:51 +0000

    feat: adiciona rotas e middleware de erro em app

--- cashforce_backend/src/app.ts
+++ cashforce_backend/src/app.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import cors from 'cors';
+import router from './routes';
+import errorMiddleware from './middlewares/error.middleware';
 
 class App {
   public app: express.Express;
@@ -12,6 +14,10 @@ class App {
   private config():void {
     this.app.use(cors());
     this.app.use(express.json());
+
+    this.app.use(router);
+
+    this.app.use(errorMiddleware);
   }
 
   public start(PORT: string | number):void {
