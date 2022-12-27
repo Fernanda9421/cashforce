ca502b575d9453855ac362bf987ea5e6077f1a2f
Fernanda Andrade de Carvalho <fer.candrade21@gmail.com>
2022-12-27 20:58:45 +0000

    feat: cria Model - Buyer

---
+++ cashforce_backend/src/database/models/Buyer.model.ts
@@ -0,0 +1,67 @@
+import { Model, DataTypes } from 'sequelize';
+import db from '.';
+import Cnpj from './Cnpj.model';
+
+class Buyer extends Model {
+  declare id: number;
+  declare name: string;
+  declare tradingName: string;
+  declare cashforceTax: string;
+  declare responsibleName: string;
+  declare responsibleEmail: string;
+  declare responsiblePosition: string;
+  declare responsiblePhone: string;
+  declare responsibleMobile: string;
+  declare website: string;
+  declare postalCode: string;
+  declare address: string;
+  declare number: string;
+  declare complement: string;
+  declare neighborhood: string;
+  declare city: string;
+  declare state: string;
+  declare phoneNumber: string;
+  declare situation: string;
+  declare situationDate: string;
+  declare createdAt: string;
+  declare updatedAt: string;
+  declare cnpjId: number;
+  declare confirm: number;
+  declare email: string;
+}
+
+Buyer.init({
+  id: { type: DataTypes.INTEGER, allowNull: false, primaryKey: true, autoIncrement: true },
+  name: { type: DataTypes.STRING, allowNull: false },
+  tradingName: { type: DataTypes.STRING },
+  cashforceTax: { type: DataTypes.STRING },
+  responsibleName: { type: DataTypes.STRING },
+  responsibleEmail: { type: DataTypes.STRING },
+  responsiblePosition: { type: DataTypes.STRING },
+  responsiblePhone: { type: DataTypes.STRING },
+  responsibleMobile: { type: DataTypes.STRING },
+  website: { type: DataTypes.STRING },
+  postalCode: { type: DataTypes.STRING },
+  address: { type: DataTypes.STRING },
+  number: { type: DataTypes.STRING },
+  complement: { type: DataTypes.STRING },
+  neighborhood: { type: DataTypes.STRING },
+  city: { type: DataTypes.STRING },
+  state: { type: DataTypes.STRING },
+  phoneNumber: { type: DataTypes.STRING },
+  situation: { type: DataTypes.STRING },
+  situationDate: { type: DataTypes.STRING },
+  createdAt: { type: DataTypes.DATE, allowNull: false },
+  updatedAt: { type: DataTypes.DATE, allowNull: false },
+  cnpjId: { type: DataTypes.INTEGER },
+  confirm: { type: DataTypes.SMALLINT, defaultValue: 1 },
+  email: { type: DataTypes.STRING },
+}, {
+  sequelize: db,
+  modelName: 'Buyer',
+  underscored: true,
+});
+
+Buyer.belongsTo(Cnpj, { foreignKey: 'cnpjId', as: 'cnpjs' });
+
+export default Buyer;
