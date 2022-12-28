f18e30e1bac24abb42674800aca830aaa072c9fd
Fernanda Andrade de Carvalho <fer.candrade21@gmail.com>
2022-12-28 22:13:18 +0000

    feat: componente Aside

---
+++ cashforce_frontend/src/components/AsidePage/AsidePage.vue
@@ -0,0 +1,35 @@
+<script setup>
+import MenuHeroVue from "./MenuHero.vue";
+import MenuItemVue from "./MenuItem.vue";
+</script>
+
+<template>
+  <aside>
+    <div class="container">
+      <MenuHeroVue />
+      <MenuItemVue />
+    </div>
+  </aside>
+</template>
+
+<style scoped>
+aside {
+  position: absolute;
+  width: 18.46vw;
+  height: 100vh;
+  left: 0px;
+  top: 0px;
+}
+
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0px;
+  position: absolute;
+  width: 18.46vw;
+  height: 144px;
+  left: 0px;
+  top: 0px;
+}
+</style>
