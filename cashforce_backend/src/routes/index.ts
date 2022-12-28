2ef73b3bb0429f2ec9361f11fd57eda83d50405c
Fernanda Andrade de Carvalho <fer.candrade21@gmail.com>
2022-12-28 10:12:35 +0000

    feat: adiciona rotas ao index

--- cashforce_backend/src/routes/index.ts
+++ cashforce_backend/src/routes/index.ts
@@ -1,8 +1,22 @@
 import { Router } from 'express';
 import buyerRouter from './buyer.router';
+import cnpjRouter from './cnpj.router';
+import offerRouter from './offer.router';
+import orderRouter from './order.router';
+import orderportionRouter from './orderportion.router';
+import providerRouter from './provider.router';
+import sponsorRouter from './sponsor.router';
+import userRouter from './user.router';
 
 const router = Router();
 
 router.use('/buyer', buyerRouter);
+router.use('/cnpj', cnpjRouter);
+router.use('/offer', offerRouter);
+router.use('/order', orderRouter);
+router.use('/orderportion', orderportionRouter);
+router.use('/provider', providerRouter);
+router.use('/sponsor', sponsorRouter);
+router.use('/user', userRouter);
 
 export default router;
