b52ba4f23c8c61d24fedb11b724adeec2671bb56
Fernanda Andrade de Carvalho <fer.candrade21@gmail.com>
2022-12-28 22:12:58 +0000

    feat: componente MenuHero

---
+++ cashforce_frontend/src/components/AsidePage/MenuHero.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container-hero">
+    <div class="box-logo">
+      <img
+        alt="Cashforce logo"
+        class="logo"
+        src="@/assets/logo.png"
+        width="133"
+        height="21"
+      />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container-hero {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0px;
+  width: 18.46vw;
+  height: 80px;
+}
+
+.box-logo {
+  position: absolute;
+  left: 24px;
+  top: 25px;
+  width: 18.46vw;
+  height: 80px;
+}
+
+/* .logo {
+  position: absolute;
+  left: 24px;
+  top: 25px;
+} */
+</style>
