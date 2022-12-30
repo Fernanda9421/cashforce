833df73c39fcc0adb2452d066cad5da646fb0283
Fernanda Andrade de Carvalho <fer.candrade21@gmail.com>
2022-12-30 11:33:05 +0000

    refactor: altera estilização de MenuItem para css externo

--- cashforce_frontend/src/components/AsidePage/MenuItem.vue
+++ cashforce_frontend/src/components/AsidePage/MenuItem.vue
@@ -1,3 +1,7 @@
+<script setup>
+import "../../styles/AsidePage/MenuItem.css";
+</script>
+
 <template>
   <div class="container-menu-item">
     <div class="selected">
@@ -18,90 +22,3 @@
     </div>
   </div>
 </template>
-
-<style scoped>
-.container-menu-item {
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  padding: 0px;
-
-  width: 18.46vw;
-  height: 64px;
-}
-
-.selected {
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  padding: 0px;
-  gap: 21px;
-
-  width: 149px;
-  height: 64px;
-}
-
-.box-selected {
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  padding: 10px 0px;
-  gap: 10px;
-
-  width: 4px;
-  height: 84px;
-}
-
-.line-selected {
-  width: 4px;
-  height: 64px;
-  background: #00ad8c;
-  border-radius: 4px;
-}
-
-.box-item {
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  padding: 0px;
-  gap: 15px;
-
-  width: 124px;
-  height: 44px;
-}
-
-.icon-item {
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  padding: 10px 0px;
-  gap: 10px;
-
-  width: 24px;
-  height: 44px;
-}
-
-.box-text-item {
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  padding: 10px 0px;
-  gap: 10px;
-
-  width: 85px;
-  height: 44px;
-}
-
-.text-item {
-  width: 85px;
-  height: 18px;
-  font-family: "DM Sans";
-  font-style: normal;
-  font-weight: 500;
-  font-size: 14px;
-  line-height: 18px;
-  display: flex;
-  align-items: center;
-  color: #00ad8c;
-}
-</style>
