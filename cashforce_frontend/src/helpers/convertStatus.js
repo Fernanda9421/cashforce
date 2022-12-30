5ec29a6b94f8c819546eb3602ab38cffc036ee9c
Fernanda Andrade de Carvalho <fer.candrade21@gmail.com>
2022-12-30 11:29:57 +0000

    feat: função auxiliar para converter orderStatus

---
+++ cashforce_frontend/src/helpers/convertStatus.js
@@ -0,0 +1,15 @@
+const status = [
+  "Pendente de confirmação",
+  "Pedido confirmado",
+  "Não reconhece o pedido",
+  "Mercadoria não recebida",
+  "Recebida com avaria",
+  "Devolvida",
+  "Recebida com devolução parcial",
+  "Recebida e confirmada",
+  "Pagamento Autorizado",
+];
+
+export function convertStatus(orderStatus) {
+  return status[Number(orderStatus)].toUpperCase();
+}
