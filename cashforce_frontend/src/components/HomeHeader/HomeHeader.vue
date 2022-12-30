17ab9327cba16f1b4184fea9df4a9389ee95ab8c
Fernanda Andrade de Carvalho <fer.candrade21@gmail.com>
2022-12-30 11:34:05 +0000

    feat: cria e estiliza header de página principal

---
+++ cashforce_frontend/src/components/HomeHeader/HomeHeader.vue
@@ -0,0 +1,22 @@
+<script setup>
+import "../../styles/HomeHeader/HomeHeader.css";
+</script>
+
+<template>
+  <div class="container-home-header">
+    <div class="box-home-header">
+      <div class="box-title">
+        <div class="box-icon">
+          <img
+            alt="Vector home header"
+            src="@/assets/vector-header.png"
+            width="24"
+            height="14"
+          />
+        </div>
+        <p class="title">Notas fiscais</p>
+      </div>
+      <p class="subtitle">Visualize as notas fiscais que você tem.</p>
+    </div>
+  </div>
+</template>
