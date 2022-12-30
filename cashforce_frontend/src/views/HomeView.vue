f9467de9140bc6825a48d61931f24abb8b40e1ff
Fernanda Andrade de Carvalho <fer.candrade21@gmail.com>
2022-12-30 11:34:58 +0000

    feat: adiciona componentes à página

--- cashforce_frontend/src/views/HomeView.vue
+++ cashforce_frontend/src/views/HomeView.vue
@@ -1,9 +1,13 @@
 <script setup>
+import HomeHeaderVue from "../components/HomeHeader/HomeHeader.vue";
 import AsidePageVue from "../components/AsidePage/AsidePage.vue";
+import MainTableVue from "../components/MainTable/MainTable.vue";
 </script>
 
 <template>
   <main>
     <AsidePageVue />
+    <HomeHeaderVue />
+    <MainTableVue />
   </main>
 </template>
