a2ce6f18a91a41cc8c86904e3b600f5e72ddd425
Fernanda Andrade de Carvalho <fer.candrade21@gmail.com>
2023-01-02 15:41:29 +0000

    feat: cria e estiliza - página não encontrada

---
+++ cashforce_frontend/src/views/NotFoundView.vue
@@ -0,0 +1,20 @@
+<script setup>
+import "../styles/NotFoundPage/NotFoundPage.css";
+</script>
+
+<template>
+  <div class="container-not-found">
+    <img
+      alt="Not found image"
+      src="@/assets/not-found.jpg"
+      width="400"
+      height="400"
+    />
+    <div class="box">
+      <p class="text">Página não encontrada</p>
+      <button @click="$router.push({ name: 'home' })" class="goHome">
+        Página Principal
+      </button>
+    </div>
+  </div>
+</template>
