2825d584890842150f8fb49d77493db8cd048ba6
Fernanda Andrade de Carvalho <fer.candrade21@gmail.com>
2023-01-03 15:16:36 +0000

    refactor: adiciona tags semânticas ao App

--- cashforce_frontend/src/App.vue
+++ cashforce_frontend/src/App.vue
@@ -4,11 +4,7 @@ import { RouterView } from "vue-router";
 
 <template>
   <header>
-    <div class="system-header">
-      <div class="box-header">
-        <div class="line-header"></div>
-      </div>
-    </div>
+    <section class="line-header" />
   </header>
 
   <RouterView />
@@ -22,23 +18,6 @@ header {
   left: 18.46vw;
   top: 0px;
   background-color: #ffffff;
-}
-
-.system-header {
-  position: absolute;
-  left: 0%;
-  right: 0%;
-  top: 0%;
-  bottom: 0%;
-  border-radius: 0px;
-}
-
-.box-header {
-  position: absolute;
-  width: 81.54vw;
-  height: 64px;
-  left: 0px;
-  top: 0px;
 }
 
 .line-header {
